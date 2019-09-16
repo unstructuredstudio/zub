7625ce1c77e5f86e603b846c83c48a542c7e3f13
srish <srishakatux@gmail.com>
2019-09-15 23:18:50 -0700

    fix code indentation

--- src/PrimaryScreen.js
+++ src/PrimaryScreen.js
@@ -20,21 +20,99 @@ import AwesomeButtonCartman from 'react-native-really-awesome-button/src/themes/
 
 export default function PrimaryScreen(props) {
   const [recording, setRecording] = React.useState(null),
-        [processing, setProcessing] = React.useState(false),
-        [curScreenNum, setCurScreenNum] = React.useState(1),
-        [nextScreenNum, setNextScreenNum] = React.useState(1),
-        [workInProgress, SetWorkInProgress] = React.useState(false),
+    [processing, setProcessing] = React.useState(false),
+    [curScreenNum, setCurScreenNum] = React.useState(1),
+    [nextScreenNum, setNextScreenNum] = React.useState(1),
+    [workInProgress, SetWorkInProgress] = React.useState(false),
 
-        [buttonTypes, setButtonTypes] = React.useState([
-          { 'button': 1, type: 'primary' },
-          { 'button': 2, type: 'enabled' },
-          { 'button': 3, type: 'enabled' },
-        ]);
+    [buttonTypes, setButtonTypes] = React.useState([
+      { 'button': 1, type: 'primary' },
+      { 'button': 2, type: 'enabled' },
+      { 'button': 3, type: 'enabled' },
+    ]);
 
 
   React.useEffect(() => {
     switchScreen();
   });
+
+  return (
+    <Fragment>
+    <SafeAreaView flex={1}>
+      <View style={styles.container}>
+      <View style={styles.containerLeft}>
+        <View style={styles.video}>
+          <VideoRecorder
+            recording={recording}
+            setRecording={setRecording}
+            processing={processing}
+            setProcessing={setProcessing}
+          />
+        </View>
+        <ProgressBar
+          recording={recording}
+          setRecording={setRecording}
+        />
+      </View>
+      <View style={styles.containerRight}>
+        <View style={styles.videoButtonContainer}>
+        <AwesomeButtonCartman
+          borderRadius={buttonTypes[0].type === 'primary' ? 5 : 0}
+          borderWidth={buttonTypes[0].type === 'primary' ? 5 : 0}
+          height={100}
+          textSize={50}
+          stretch={true}
+          type={buttonTypes[0].type}
+          onPress={() => {
+            setNextScreenNum(1);
+          }}
+          >
+          <Text style={styles.buttonText}>1</Text>
+        </AwesomeButtonCartman>
+        </View>
+        <View style={styles.videoButtonContainer}>
+          <AwesomeButtonCartman
+          height={100}
+          stretch={true}
+          borderRadius={buttonTypes[1].type === 'primary' ? 5 : 0}
+          borderWidth={buttonTypes[1].type === 'primary' ? 5 : 0}
+          type={buttonTypes[1].type}
+          onPress={() => {
+            setNextScreenNum(2);
+          }}
+          ><Text style={styles.buttonText}>2</Text></AwesomeButtonCartman>
+        </View>
+        <View style={styles.videoButtonContainer}>
+          <AwesomeButtonCartman
+          height={100}
+          stretch={true}
+          borderRadius={buttonTypes[2].type === 'primary' ? 5 : 0}
+          borderWidth={buttonTypes[2].type === 'primary' ? 5 : 0}
+          type={buttonTypes[2].type}
+          onPress={() => {
+            setNextScreenNum(3);
+          }}
+          >
+          <Text style={styles.buttonText}>3</Text></AwesomeButtonCartman>
+        </View>
+        <View style={styles.recordButtonContainer}>
+          <AwesomeButtonCartman
+            borderRadius={50}
+            height={100}
+            stretch={true}
+            type="secondary"
+            onPress={() => setRecording(!recording)}
+          >
+          {
+            recording ? 'STOP' : 'RECORD'
+          }
+          </AwesomeButtonCartman>
+        </View>
+      </View>
+      </View>
+    </SafeAreaView>
+    </Fragment>
+  );
 
   function switchScreen() {
     if (nextScreenNum === curScreenNum || workInProgress ||
@@ -48,85 +126,6 @@ export default function PrimaryScreen(props) {
     setButtonTypes(buttonTypes);
     setCurScreenNum(nextScreenNum);
   }
-
-        return (
-            <Fragment>
-            <SafeAreaView flex={1}>
-                    <View style={styles.container}>
-                    <View style={styles.containerLeft}>
-                        <View style={styles.video}>
-                            <VideoRecorder
-                              recording={recording}
-                              setRecording={setRecording}
-                              processing={processing}
-                              setProcessing={setProcessing}
-                            />
-                        </View>
-                        <ProgressBar
-                          recording={recording}
-                          setRecording={setRecording}
-                        />
-                    </View>
-                    <View style={styles.containerRight}>
-                        <View style={styles.videoButtonContainer}>
-
-                        <AwesomeButtonCartman
-                            borderRadius={buttonTypes[0].type === 'primary' ? 5 : 0}
-                            borderWidth={buttonTypes[0].type === 'primary' ? 5 : 0}
-                            height={100}
-                            textSize={50}
-                            stretch={true}
-                            type={buttonTypes[0].type}
-                            onPress={() => {
-                              setNextScreenNum(1);
-                            }}
-                            >
-                            <Text style={styles.buttonText}>1</Text>
-                        </AwesomeButtonCartman>
-                        </View>
-                        <View style={styles.videoButtonContainer}>
-                            <AwesomeButtonCartman
-                            height={100}
-                            stretch={true}
-                            borderRadius={buttonTypes[1].type === 'primary' ? 5 : 0}
-                            borderWidth={buttonTypes[1].type === 'primary' ? 5 : 0}
-                            type={buttonTypes[1].type}
-                            onPress={() => {
-                              setNextScreenNum(2);
-                            }}
-                            ><Text style={styles.buttonText}>2</Text></AwesomeButtonCartman>
-                        </View>
-                        <View style={styles.videoButtonContainer}>
-                            <AwesomeButtonCartman
-                            height={100}
-                            stretch={true}
-                            borderRadius={buttonTypes[2].type === 'primary' ? 5 : 0}
-                            borderWidth={buttonTypes[2].type === 'primary' ? 5 : 0}
-                            type={buttonTypes[2].type}
-                            onPress={() => {
-                              setNextScreenNum(3);
-                            }}
-                            >
-                            <Text style={styles.buttonText}>3</Text></AwesomeButtonCartman>
-                        </View>
-                        <View style={styles.recordButtonContainer}>
-                        <AwesomeButtonCartman
-                          borderRadius={50}
-                          height={100}
-                          stretch={true}
-                          type="secondary"
-                          onPress={() => setRecording(!recording)}
-                        >
-                        {
-                          recording ? 'STOP' : 'RECORD'
-                        }
-                        </AwesomeButtonCartman>
-                        </View>
-                    </View>
-                    </View>
-            </SafeAreaView>
-            </Fragment>
-        );
 }
 
 // Remove Navigation Header
