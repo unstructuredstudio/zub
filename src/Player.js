67f6389151f12e802cb45f5d5b68c7df158084c6
Suchakra <suchakra@gmail.com>
2019-09-24 20:53:33 -0700

    Major rework of player and recorder states
      * We now have global app states so that components are aware and can respond to changes
      * Redefine playing and recording states
      * Filenames are now passed in props of each screen
      * Use functional component for VideoPlayer
    
    The workflow (only for now) is as follows:
    
      * As the app starts, each screen is at NONE state (nothing recorded)
      * You can click each screen button and press record button
      * As soon as your finish recording, camera view changes to player view and starts playing
      * Do it for all screens
      * Next time you click any screen we see previews playing in player view
      * Clicking on recording changes state to RECORDING and old recording gets overwritten
      * States for each screen are preserved

--- src/Player.js
+++ src/Player.js
@@ -13,80 +13,69 @@ import SoundRecorder from 'react-native-sound-recorder';
 import { RNFFmpeg } from 'react-native-ffmpeg';
 import RNFS from 'react-native-fs';
 
-export default class VideoPlayer extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      recording: false,
-    };
-    this.fileNum = props.fileNum;
-    this.startAudioRecording = this.startAudioRecording.bind(this);
-    this.stopAudioRecording = this.stopAudioRecording.bind(this);
-  }
+export default function VideoPlayer(props) {
+  const [recording, setRecording] = React.useState(false);
+  const { fileNum, fileUri } = props;
 
-  render() {
-    const { recording } = this.state;
-
-    let button = (
-      <TouchableOpacity onPress={this.startAudioRecording.bind(this)}
-        style={styles.capture}>
-        <Text style={{ fontSize: 14 }}> RECORD </Text>
-      </TouchableOpacity>
-    );
-
-    if (recording) {
-      button = (
-        <TouchableOpacity
-          onPress={this.stopAudioRecording.bind(this)}
-          style={styles.capture}
-        >
-          <Text style={{ fontSize: 14 }}> STOP </Text>
-        </TouchableOpacity>
-      );
-    }
-
-    return (
-      <View style={styles.videoContainer}>
-        <Video source={{ uri: global.clipUri }}
-          ref={(ref) => {
-            this.player = ref;
-          }}
-          onBuffer={this.onBuffer}                // Callback when remote video is buffering
-          onEnd={this.onEnd}                      // Callback when playback finishes
-          onError={this.videoError}
-          muted={false}
-          style={styles.backgroundVideo} />
-
-        <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
-        {button}
-        </View>
-      </View>
-    );
-  }
-
-  async startAudioRecording() {
-    this.setState({ recording: true });
-    await SoundRecorder.start(RNFS.CachesDirectoryPath + '/audio_' + this.fileNum + '.mp4')
+  async function startAudioRecording() {
+    setRecording(true);
+    await SoundRecorder.start(RNFS.CachesDirectoryPath + '/audio_' + fileNum + '.mp4')
     .then(function() {
       console.log('started recording');
     });
   }
 
-  stopAudioRecording() {
-    this.setState({ recording: false });
-    const destPath = RNFS.CachesDirectoryPath + '/output_' + this.fileNum + '.mp4';
+  function stopAudioRecording() {
+    setRecording(false);
+    const destPath = RNFS.CachesDirectoryPath + '/output_' + fileNum + '.mp4';
 
     SoundRecorder.stop()
     .then(function(audio) {
       console.log('Stopped recording, audio file saved at: ' + audio.path);
 
       //Combine audio and video
-      RNFFmpeg.execute('-i ' + global.clipUri + ' -i ' + audio.path + ' -c copy ' + destPath, ' ')
+      RNFFmpeg.execute('-i ' + fileUri + ' -i ' + audio.path + ' -c copy ' + destPath, ' ')
       .then(media => console.log('FFmpeg process exited with rc ' + media.rc));
     }).catch(function(error) {
       console.log('An error occured while stoping the recording: ' + error);
     });
   }
+
+  let button = (
+    <TouchableOpacity onPress={startAudioRecording}
+      style={styles.capture}>
+      <Text style={{ fontSize: 14 }}> RECORD </Text>
+    </TouchableOpacity>
+  );
+
+  if (recording) {
+    button = (
+      <TouchableOpacity
+        onPress={stopAudioRecording}
+        style={styles.capture}
+      >
+        <Text style={{ fontSize: 14 }}> STOP </Text>
+      </TouchableOpacity>
+    );
+  }
+
+  return (
+    <View style={styles.videoContainer}>
+      <Video source={{ uri: fileUri }}
+        ref={(ref) => {
+          this.player = ref;
+        }}
+        onBuffer={this.onBuffer}                // Callback when remote video is buffering
+        onEnd={this.onEnd}                      // Callback when playback finishes
+        onError={this.videoError}
+        muted={false}
+        style={styles.backgroundVideo} />
+
+      <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+      {button}
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
