2dbf11fc3620e92b37cc94ed4b487181cf9a9969
srish <srishakatux@gmail.com>
2019-12-20 17:55:16 -0800

    eslint and indentation fixes

--- src/ShareScreen.js
+++ src/ShareScreen.js
@@ -13,138 +13,139 @@ import AwesomeButtonRick from 'react-native-really-awesome-button/src/themes/ric
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faDownload, faPlay, faChevronLeft, faPause, faRedo, faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import { saveToCameraRoll } from './Utils';
-import Modal from "react-native-modal";
+import Modal from 'react-native-modal';
 
 export default function ShareScreen(props) {
     const { navigate } = props.navigation,
         [ videoPaused, setVideoPaused ] = React.useState(true),
         [ isSaving, setIsSaving ] = React.useState(false),
-        [isModalVisible, setIsModalVisible] = React.useState(false);
+        [ isModalVisible, setIsModalVisible ] = React.useState(false);
 
     let zubVideoUrl = props.navigation.getParam('zubVideoUrl');
 
     return (
         <Fragment>
-        <SafeAreaView flex={1}>
-            <View style={styles.container}>
-                <View style={styles.videoContainer}>
-                    <Video
-                        source={{ uri: zubVideoUrl }}
-                        style={styles.backgroundVideo}
-                        resizeMode="cover"
-                        paused={videoPaused}
-                        repeat={true}
-                        onEnd={() => {
-                            setVideoPaused(!videoPaused);
-                        }}
-                    />
-                    <View style={styles.backButton}>
-                        <AwesomeButtonRick
-                            borderRadius={50}
-                            textSize={30}
-                            height={50}
-                            stretch={true}
-                            type="secondary"
-                            onPress={() => {
-                                navigate('PrimaryScreen');
-                            }}
-                            title="Back">
-                            <FontAwesomeIcon
-                                icon={ faChevronLeft }
-                                color={ '#349890' }
-                            />
-                        </AwesomeButtonRick>
-                    </View>
-                    <View style={styles.playButton}>
-                        <AwesomeButtonRick
-                            borderRadius={50}
-                            height={50}
-                            textSize={30}
-                            stretch={true}
-                            type="secondary"
-                            onPress={() => {
+            <SafeAreaView flex={1}>
+                <View style={styles.container}>
+                    <View style={styles.videoContainer}>
+                        <Video
+                            source={{ uri: zubVideoUrl }}
+                            style={styles.backgroundVideo}
+                            resizeMode="cover"
+                            paused={videoPaused}
+                            repeat={true}
+                            onEnd={() => {
                                 setVideoPaused(!videoPaused);
                             }}
+                        />
+                        <View style={styles.backButton}>
+                            <AwesomeButtonRick
+                                borderRadius={50}
+                                textSize={30}
+                                height={50}
+                                stretch={true}
+                                type="secondary"
+                                onPress={() => {
+                                    navigate('PrimaryScreen');
+                                }}
+                                title="Back"
                             >
-                            { videoPaused ?
-                            <FontAwesomeIcon
-                                icon={ faPlay }
-                                color={ '#349890' }
-                            /> :
-                            <FontAwesomeIcon
-                                icon={ faPause }
-                                color={ '#349890' }
-                            />
-                            }
-                        </AwesomeButtonRick>
-                    </View>
-                    <View style={styles.downloadButton}>
-                        <AwesomeButtonRick
-                            borderRadius={50}
-                            height={50}
-                            textSize={30}
-                            width={isSaving ? 150:110}
-                            type={isSaving ? "disabled": "anchor"}
-                            onPress={() => {
-                                if(!isSaving) {
-                                    setIsSaving(true);
-                                    if (saveToCameraRoll(zubVideoUrl)) {
-                                        setIsSaving(false);
-                                        setIsModalVisible(!isModalVisible);
-                                    }
-                                }
-                            }}
-                            title="Save">
-                            { isSaving ?
-                            <Text style={[styles.buttonFontStyle, styles.saveDisabledText]}>
-                            <FontAwesomeIcon
-                                icon={ faRedo }
-                                color={ '#c7e8ae' }
-                                size={20}
-                            /> SAVING... </Text> :
-                            <Text style={[styles.buttonFontStyle, styles.saveText]}>
-                            <FontAwesomeIcon
-                                icon={ faDownload }
-                                color={ '#34711f' }
-                                size={20}
-                            /> SAVE
-                            </Text>
-                            }
-                        </AwesomeButtonRick>
-                    </View>
-                </View>
-                <Modal isVisible={isModalVisible}>
-                    <View style={styles.infoBox}>
-                        <View style={styles.infoBoxContent}>
-                        <Text style={[styles.infoBoxTitleView, styles.infoBoxTitleText]}>Video saved!</Text>
-                        <View style={{padding: 15}}>
-                            <Text style={styles.infoBoxSubtitleText}> You can view the saved video in your phone's gallery. </Text>
-                            <View style={{alignItems: "flex-end"}}>
+                                <FontAwesomeIcon
+                                    icon={ faChevronLeft }
+                                    color={ '#349890' }
+                                />
+                            </AwesomeButtonRick>
+                        </View>
+                        <View style={styles.playButton}>
                             <AwesomeButtonRick
                                 borderRadius={50}
                                 height={50}
                                 textSize={30}
-                                width={90}
-                                type="anchor"
+                                stretch={true}
+                                type="secondary"
                                 onPress={() => {
-                                    setIsSaving(false);
-                                    setIsModalVisible(!isModalVisible);
-                                }}>
-                                <Text style={[styles.buttonFontStyle, styles.okText]}>
+                                    setVideoPaused(!videoPaused);
+                                }}
+                                >
+                                { videoPaused ?
                                 <FontAwesomeIcon
-                                    icon={ faCheckCircle }
+                                    icon={ faPlay }
+                                    color={ '#349890' }
+                                /> :
+                                <FontAwesomeIcon
+                                    icon={ faPause }
+                                    color={ '#349890' }
+                                />
+                                }
+                            </AwesomeButtonRick>
+                        </View>
+                        <View style={styles.downloadButton}>
+                            <AwesomeButtonRick
+                                borderRadius={50}
+                                height={50}
+                                textSize={30}
+                                width={isSaving ? 150 : 110}
+                                type={isSaving ? 'disabled' : 'anchor'}
+                                onPress={() => {
+                                    if (!isSaving) {
+                                        setIsSaving(true);
+                                        if (saveToCameraRoll(zubVideoUrl)) {
+                                            setIsSaving(false);
+                                            setIsModalVisible(!isModalVisible);
+                                        }
+                                    }
+                                }}
+                                title="Save">
+                                { isSaving ?
+                                <Text style={[styles.buttonFontStyle, styles.saveDisabledText]}>
+                                <FontAwesomeIcon
+                                    icon={ faRedo }
+                                    color={ '#c7e8ae' }
+                                    size={20}
+                                /> SAVING... </Text> :
+                                <Text style={[styles.buttonFontStyle, styles.saveText]}>
+                                <FontAwesomeIcon
+                                    icon={ faDownload }
                                     color={ '#34711f' }
                                     size={20}
-                                /> OK
+                                /> SAVE
                                 </Text>
+                                }
                             </AwesomeButtonRick>
-                            </View>
-                        </View>
                         </View>
                     </View>
-                </Modal>
-            </View>
-        </SafeAreaView>
+                    <Modal isVisible={isModalVisible}>
+                        <View style={styles.infoBox}>
+                            <View style={styles.infoBoxContent}>
+                                <Text style={[styles.infoBoxTitleView, styles.infoBoxTitleText]}>Video saved!</Text>
+                                <View style={{padding: 15}}>
+                                    <Text style={styles.infoBoxSubtitleText}> You can view the saved video in your phone's gallery. </Text>
+                                    <View style={{alignItems: 'flex-end'}}>
+                                        <AwesomeButtonRick
+                                        borderRadius={50}
+                                        height={50}
+                                        textSize={30}
+                                        width={90}
+                                        type="anchor"
+                                        onPress={() => {
+                                            setIsSaving(false);
+                                            setIsModalVisible(!isModalVisible);
+                                        }}>
+                                        <Text style={[styles.buttonFontStyle, styles.okText]}>
+                                        <FontAwesomeIcon
+                                            icon={ faCheckCircle }
+                                            color={ '#34711f' }
+                                            size={20}
+                                        /> OK
+                                        </Text>
+                                    </AwesomeButtonRick>
+                                    </View>
+                                </View>
+                            </View>
+                        </View>
+                    </Modal>
+                </View>
+            </SafeAreaView>
         </Fragment>
     );
 }
@@ -159,14 +160,14 @@ const styles = StyleSheet.create({
     backgroundVideo: {
         flex: 1,
         borderRadius: 10,
-        backgroundColor: "black"
+        backgroundColor: 'black',
     },
     videoContainer: {
         flex: 1,
     },
     buttonFontStyle: {
         fontSize: 20,
-        fontFamily: Platform.OS === "ios" ? 'd puntillas D to tiptoe': 'Dpuntillas-Regular',
+        fontFamily: Platform.OS === 'ios' ? 'd puntillas D to tiptoe' : 'Dpuntillas-Regular',
     },
     backButton: {
         position: 'absolute',
@@ -200,7 +201,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         borderRadius: 7,
         borderColor: '#ffc200',
-        borderWidth: 5
+        borderWidth: 5,
     },
     infoBoxTitleView: {
         backgroundColor: '#ee3355',
@@ -212,7 +213,7 @@ const styles = StyleSheet.create({
     infoBoxTitleText: {
         fontSize: 25,
         color:'#e1dfe2',
-        fontFamily: Platform.OS === "ios" ? 'd puntillas D to tiptoe': 'Dpuntillas-Regular',
+        fontFamily: Platform.OS === 'ios' ? 'd puntillas D to tiptoe' : 'Dpuntillas-Regular',
     },
     infoBoxSubtitleText: {
         fontSize: 20,
