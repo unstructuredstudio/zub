b38095840c25381ea3e47e8c68f0ed926990637b
Suchakra Sharma <suchakra@gmail.com>
2020-02-23 16:58:50 -0800

    Add video title clips and merge them in videos

--- src/Utils.js
+++ src/Utils.js
@@ -7,7 +7,7 @@
  */
 
 import {PermissionsAndroid, Platform} from 'react-native';
-import RNFS from 'react-native-fs';
+import RNFS, { existsAssets } from 'react-native-fs';
 import CameraRoll from '@react-native-community/cameraroll';
 import {RNFFmpeg} from 'react-native-ffmpeg';
 
@@ -131,18 +131,43 @@ export async function mergeVideos(playersState) {
   const im0 = cacheDir + '/im_0.ts';
   const im1 = cacheDir + '/im_1.ts';
   const im2 = cacheDir + '/im_2.ts';
+  const t0 = cacheDir + '/t0.ts';
+  const t1 = cacheDir + '/t1.ts';
+  const t2 = cacheDir + '/t2.ts';
+
+  // Synfig animations were first geenrated as MPEG4-part 2 (ffmpeg) then converted to h264
+  // via "ffmpeg -i title-materials.mp4 -vcodec libx264 title-materials-h264.mp4"
+
+  // Copy the titles to cacheDir
+  // TODO: Ensure assets are physically present in iOS and are then copied. Then, test!
+  if (Platform.OS === 'android') {
+    await RNFS.copyFileAssets('videos/title-motivation-h264.mp4', cacheDir + '/title0.mp4');
+    await RNFS.copyFileAssets('videos/title-materials-h264.mp4', cacheDir + '/title1.mp4');
+    await RNFS.copyFileAssets('videos/title-making-h264.mp4', cacheDir + '/title2.mp4');
+  } else if (Platform.OS === 'ios'){
+    await RNFS.copyAssetsFileIOS('videos/title-motivation-h264.mp4', cacheDir + '/title0.mp4', 0, 0);
+    await RNFS.copyAssetsFileIOS('videos/title-motivation-h264.mp4', cacheDir + '/title1.mp4', 0, 0);
+    await RNFS.copyAssetsFileIOS('videos/title-motivation-h264.mp4', cacheDir + '/title2.mp4', 0, 0);
+  }
+
+  const title0 = cacheDir + '/title0.mp4';
+  const title1 = cacheDir + '/title1.mp4';
+  const title2 = cacheDir + '/title2.mp4';
+
   const zubVid = cacheDir + '/zub_video.mp4';
   const ffTransCmd = ' -c copy -bsf:v h264_mp4toannexb -f mpegts ';
   const ffConCmd = ' -c copy -bsf:a aac_adtstoasc ';
-  let output = '';
 
   const promise1 = deleteMediaFile(zubVid);
   const promise2 = deleteMediaFile(im0);
   const promise3 = deleteMediaFile(im1);
   const promise4 = deleteMediaFile(im2);
+  const promise5 = deleteMediaFile(t0);
+  const promise6 = deleteMediaFile(t1);
+  const promise7 = deleteMediaFile(t2);
 
   await Promise.all([promise1, promise2, promise3,
-    promise4]).then(function(res) {
+    promise4, promise5, promise6, promise7]).then(function(res) {
     console.log('Existing videos deleted ' + res);
   });
 
@@ -151,6 +176,18 @@ export async function mergeVideos(playersState) {
   * support is added, the FFMPEG executions below can be done in parallel
   * like the media file deletions above.
   */
+
+  // Convert title videos to intermediate transport streams
+  await RNFFmpeg.execute('-i ' + title0 + ffTransCmd + t0)
+      .then((title) => console.log(title.rc));
+
+  await RNFFmpeg.execute('-i ' + title1 + ffTransCmd + t1)
+      .then((title) => console.log(title.rc));
+  
+  await RNFFmpeg.execute('-i ' + title2 + ffTransCmd + t2)
+      .then((title) => console.log(title.rc));
+
+  // Convert remaining videos to intermediate transport streams
   await RNFFmpeg.execute('-i ' + video0 + ffTransCmd + im0)
       .then((media0) => console.log(media0.rc));
 
@@ -160,8 +197,8 @@ export async function mergeVideos(playersState) {
   await RNFFmpeg.execute('-i ' + video2 + ffTransCmd + im2)
       .then((media2) => console.log(media2.rc));
 
-  await RNFFmpeg.execute('-i concat:' + im0 + '|' +
-    im1 + '|' + im2 + ffConCmd + zubVid)
+  await RNFFmpeg.execute('-i concat:' + t0 + '|' + im0 + '|' + t1 + '|' +
+    im1 + '|' + t2 + '|' + im2 + ffConCmd + zubVid)
       .then(function(mediaZub) {
         if (mediaZub.rc === 0) {
           output = zubVid;
